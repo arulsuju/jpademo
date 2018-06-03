14e5fe872f61ec4093dcbfa6111cdabae09edd33
Arul Suju <arulsuju@yahoo.co.in>
2018-06-04 00:13:12 +0530

    Initial commit

---
+++ src/main/java/come/jwl/org/jwl/JwlApplication.java
@@ -0,0 +1,12 @@
+package come.jwl.org.jwl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwlApplication.class, args);
+    }
+}
---
+++ src/test/java/come/jwl/org/jwl/JwlApplicationTests.java
@@ -0,0 +1,16 @@
+package come.jwl.org.jwl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JwlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
